64f0e8a2eb55e2e0f49a82e5cc1c4099e17011f3
user <user@DESKTOP-SR7V1OH>
2024-04-06 17:14:55 +0200

    Dodaj pliki projektów.

---
+++ info_app/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using info_app.Core;
+using System;
+
+namespace info_app.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public HomeViewModel HomeVM { get; set; }
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            CurrentView = HomeVM;
+        }
+    }
+}
