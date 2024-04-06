eafa4e36056b56eb36fd52fdd024d40f6533d367
Qutly <maciej.koscinski12@gmail.com>
2024-04-07 00:07:10 +0200

    xdd

--- info_app/MVVM/ViewModel/MainViewModel.cs
+++ info_app/MVVM/ViewModel/MainViewModel.cs
@@ -6,7 +6,23 @@ namespace info_app.MVVM.ViewModel
     class MainViewModel : ObservableObject
     {
 
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand GeneralViewCommand { get; set; }
+        public RelayCommand HealthViewCommand { get; set; }
+        public RelayCommand ScienceViewCommand { get; set; }
+        public RelayCommand TechnologyViewCommand { get; set; }
+        public RelayCommand EntertainmentViewCommand { get; set; }
+
+
         public HomeViewModel HomeVM { get; set; }
+
+        public GeneralViewModel GeneralVM { get; set; }
+        public HealthViewModel HealthVM { get; set; }
+        public ScienceViewModel ScienceVM { get; set; }
+        public TechnologyViewModel TechnologyVM { get; set; }
+        public EntertainmentViewModel EntertainmentVM { get; set; }
+
+
         private object _currentView;
 
         public object CurrentView
@@ -21,7 +37,44 @@ namespace info_app.MVVM.ViewModel
         public MainViewModel()
         {
             HomeVM = new HomeViewModel();
+            GeneralVM = new GeneralViewModel();
+            HealthVM = new HealthViewModel();
+            ScienceVM = new ScienceViewModel();
+            TechnologyVM = new TechnologyViewModel();
+            EntertainmentVM = new EntertainmentViewModel();
+
+
             CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            GeneralViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = GeneralVM;
+            });
+
+            HealthViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HealthVM;
+            });
+
+            ScienceViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = ScienceVM;
+            });
+
+            TechnologyViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = TechnologyVM;
+            });
+
+            EntertainmentViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = EntertainmentVM;
+            });
         }
     }
 }
